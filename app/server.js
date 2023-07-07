961adc82117f504636b9c78733e2df0eccb3ce58
fuyaozhao1018 <fuyaozhao1018@gmail.com>
2023-07-06 23:55:44 -0700

    comments

--- app/server.js
+++ app/server.js
@@ -1,11 +1,15 @@
+//server side
 const app = require('express')();
 const server = require('http').createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 const PORT = 8000;
+
+
 app.get('/', (req,res) =>{
     res.sendFile(__dirname + '/index.html');
 });
+
 
 io.on('connection', socket => {
     console.log('Someone entered Chat Room');
@@ -18,7 +22,6 @@ io.on('connection', socket => {
         console.log('Someone Left Chat Room');
     });
 });
-
 
 //http://localhost:8000/?
 server.listen(PORT, () => console.log('Sever is on'));
