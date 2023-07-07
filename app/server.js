69ce30297b0736bb64f3cb783d7bb016ccd85f5c
fuyaozhao1018 <fuyaozhao1018@gmail.com>
2023-07-06 23:11:34 -0700

    added style

--- app/server.js
+++ app/server.js
@@ -2,16 +2,17 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
-
+const PORT = 8000;
 app.get('/', (req,res) =>{
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', socket => {
     console.log('Someone entered Chat Room');
+    
     socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-    console.log(msg);
+        io.emit('chat message', msg);
+        console.log(msg);
     });
     socket.on('disconnect', ()=> {
         console.log('Someone Left Chat Room');
@@ -20,5 +21,5 @@ io.on('connection', socket => {
 
 
 //http://localhost:8000/?
-server.listen('8000', () => console.log('Sever is on'));
+server.listen(PORT, () => console.log('Sever is on'));
 
